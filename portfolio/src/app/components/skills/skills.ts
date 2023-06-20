63f140ac2564f5e09bb63780778b8e7b28498595
kaioruan <kaio.ruan@souunit.com.br>
2023-06-19 21:33:42 -0300

    create component Skills ands Abilities

---
+++ portfolio/src/app/components/skills/skills.ts
@@ -0,0 +1,67 @@
+export const skills = [
+  {
+      "name": "JavaScript",
+      "icon": "https://img.icons8.com/color/48/000000/javascript--v1.png"
+  },
+  {
+      "name": "TypeScript",
+      "icon": "https://img.icons8.com/color/48/000000/typescript--v1.png"
+  },
+  {
+      "name": "Docker",
+      "icon": "https://img.icons8.com/fluency/48/000000/docker.png"
+  },
+  {
+      "name": "ReactJS",
+      "icon": "https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/000000/external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo.png"
+  },
+  {
+      "name": "AngularJS",
+      "icon": "https://img.icons8.com/color/48/000000/angularjs--v1.png"
+  },
+  {
+      "name": "Redux",
+      "icon": "https://img.icons8.com/color/48/000000/redux.png"
+  },
+  {
+      "name": "HTML5",
+      "icon": "https://img.icons8.com/color/48/000000/html-5--v1.png"
+  },
+  {
+      "name": "CSS3",
+      "icon": "https://img.icons8.com/color/48/000000/css3.png"
+  },
+  {
+      "name": "ExpressJS",
+      "icon": "https://img.icons8.com/fluency/48/000000/node-js.png"
+  },
+  {
+      "name": "NodeJS",
+      "icon": "https://img.icons8.com/color/48/000000/nodejs.png"
+  },
+  {
+      "name": "Java",
+      "icon": "https://img.icons8.com/color/48/000000/java-coffee-cup-logo--v1.png"
+  },
+  {
+      "name": "MongoDB",
+      "icon": "https://img.icons8.com/color/48/000000/mongodb.png"
+  },
+  {
+      "name": "MySQL",
+      "icon": "https://img.icons8.com/color/48/000000/mysql-logo.png"
+  },
+
+  {
+      "name": "Git",
+      "icon": "https://img.icons8.com/color/48/000000/git.png"
+  },
+  {
+      "name": "GitHub",
+      "icon": "https://img.icons8.com/glyph-neue/48/ffffff/github.png"
+  },
+  {
+      "name": "Jest",
+      "icon": "https://img.icons8.com/external-tal-revivo-color-tal-revivo/24/external-jest-can-collect-code-coverage-information-from-entire-projects-logo-color-tal-revivo.png"
+  } 
+]
