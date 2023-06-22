207456eab49f5e73b75e99e2516be50f2a9651e9
kaioruan <kaio.ruan@souunit.com.br>
2023-06-22 13:31:11 -0300

    Feat: testing headerComponent all elements navigate

---
+++ portfolio/src/app/components/header/header.component.spec.ts
@@ -0,0 +1,84 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HeaderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('Header criado', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('deve inicializar com o menu fechado', () => {
+    expect(component.isMenuOpen).toBe(false);
+  });
+  it('Ao iniciar com o menu fechado, tem que apresentar o Header contendo Inicio, Sobre, Skills, Formação, Projetos e Contato', () => {
+    const menuElement = fixture.nativeElement.querySelector('.menu');
+    expect(component.isMenuOpen).toBe(false);
+    expect(menuElement.textContent).toContain('Inicio');
+    expect(menuElement.textContent).toContain('Sobre');
+    expect(menuElement.textContent).toContain('Skills');
+    expect(menuElement.textContent).toContain('Formação');
+    expect(menuElement.textContent).toContain('Projetos');
+    expect(menuElement.textContent).toContain('Contato');
+  });
+  it('deve abrir o menu quando o botão burger for clicado', () => {
+    const burgerElement = fixture.nativeElement.querySelector('.burger');
+    burgerElement.click();
+    expect(component.isMenuOpen).toBe(true);
+  });
+
+  it('deve fechar o menu quando o botão burger for clicado', () => { 
+    const burgerElement = fixture.nativeElement.querySelector('.burger');
+    burgerElement.click();
+    burgerElement.click();
+    expect(component.isMenuOpen).toBe(false);
+  });
+  it('Ao clicar no menu, aparece o texto renderizado em tag a Inicio', () => {
+
+    const menuElement = fixture.nativeElement.querySelector('.menu');
+    menuElement.click();
+    expect(menuElement.textContent).toContain('Inicio');
+  });
+  it('Ao clicar no menu, aparece o texto renderizado em tag a Sobre', () => {
+    
+    const menuElement = fixture.nativeElement.querySelector('.menu');
+    menuElement.click();
+    expect(menuElement.textContent).toContain('Sobre');
+  });
+  it('Ao clicar no menu, aparece o texto renderizado em tag a Skills', () => {
+    const menuElement = fixture.nativeElement.querySelector('.menu');
+    menuElement.click();
+    expect(menuElement.textContent).toContain('Skills');
+  });
+  it('Ao clicar no menu, aparece o texto renderizado em tag a Formação', () => {
+    const menuElement = fixture.nativeElement.querySelector('.menu');
+    menuElement.click();
+    expect(menuElement.textContent).toContain('Formação');
+  });
+
+  it('Ao clicar no menu, aparece o texto renderizado em tag a Projetos', () => {
+    const menuElement = fixture.nativeElement.querySelector('.menu');
+    menuElement.click();
+    expect(menuElement.textContent).toContain('Projetos');
+  });
+  it('Ao clicar no menu, aparece o texto renderizado em tag a Contato', () => {
+    const menuElement = fixture.nativeElement.querySelector('.menu');
+    menuElement.click();
+    expect(menuElement.textContent).toContain('Contato');
+  });
+});
